89349cfc0d6db988240528d8a475c76e3049e9c9
edu-matsu-devops <eduardo.kmatsubara@gmail.com>
2025-09-01 23:56:00 -0300

    Update main.py
    Adicionei uma forma para criar um novo usuário.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
@@ -8,8 +8,20 @@ async def root():
 
 @app.get("/usuarios")
 def listar_usuarios():
-    return [
+    return usuarios_db
+
+class Usuario(BaseModel):
+    id: int
+    nome: str
+    email:str
+
+usuarios_db = [
         {"id": 1, "nome": "Eduardo", "email": "edu@gmail.com"},
         {"id": 2, "nome": "Ana", "email": "ana@gmail.com"},
         {"id": 3, "nome": "Maria", "email": "maria@gmail.com"}
-    ]
+]
+
+@app.post("/usuarios")
+def criar_usuario(usuario: Usuario):
+    usuarios_db.append(usuario.dict())
+    return {"mensagem": "Usuário criado com sucesso", "usuario": usuario}
