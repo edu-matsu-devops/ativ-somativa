0c42970d695f18d59b45def46e10899a9d83b79f
edu-matsu-devops <eduardo.kmatsubara@gmail.com>
2025-09-01 23:24:19 -0300

    Alteração endpoint
    Alterei o endpoint de usuario para usuarios e adicionei um campo email.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/usuario")
+@app.get("/usuarios")
 def listar_usuarios():
     return [
-        {"id": 1, "nome": "Eduardo"},
-        {"id": 2, "nome": "Ana"},
-        {"id": 3, "nome": "Maria"}
+        {"id": 1, "nome": "Eduardo", "email": "edu@gmail.com"},
+        {"id": 2, "nome": "Ana", "email": "ana@gmail.com"},
+        {"id": 3, "nome": "Maria", "email": "maria@gmail.com"}
     ]
